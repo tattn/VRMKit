2e1d3ef8f99af85ad4bee41ede7dece9a5c58518
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2018-09-08 20:53:42 +0900

    Initial commit

---
+++ Sources/VRMKit.h
@@ -0,0 +1,19 @@
+//
+//  VRMKit.h
+//  VRMKit
+//
+//  Created by Tatsuya Tanaka on 20180908.
+//  Copyright © 2018年 tattn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VRMKit.
+FOUNDATION_EXPORT double VRMKitVersionNumber;
+
+//! Project version string for VRMKit.
+FOUNDATION_EXPORT const unsigned char VRMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VRMKit/PublicHeader.h>
+
+
