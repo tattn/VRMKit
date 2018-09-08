823a2d79cd2fcfe3c5dc9a18d9322e3e676a5d3c
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2018-09-09 00:16:04 +0900

    Add GLTF & Sampler

--- Sources/VRM/GLTF.swift
+++ Sources/VRM/GLTF.swift
@@ -9,5 +9,45 @@
 import Foundation
 
 public struct GLTF: Codable {
-    
+    public let extensionsUsed: [String]?
+    public let extensionsRequired: [String]?
+    public let accessors: [Accessor]?
+    public let animations: [Animation]?
+    public let asset: Asset
+    public let buffers: [Buffer]?
+    public let bufferViews: [BufferView]?
+    public let cameras: [Camera]?
+    public let images: [Image]?
+    public let materials: [Material]?
+    public let meshes: [Mesh]?
+    public let nodes: [Node]?
+    public let samplers: [Sampler]?
+    let _scene: Int?
+    public var scene: Int { return _scene ?? 0 }
+    public let scenes: [Scene]?
+    public let skins: [Skin]?
+    public let textures: [Texture]?
+    public let extensions: Extensions?
+    public let extras: Extensions?
+    private enum CodingKeys: String, CodingKey {
+        case extensionsUsed
+        case extensionsRequired
+        case accessors
+        case animations
+        case asset
+        case buffers
+        case bufferViews
+        case cameras
+        case images
+        case materials
+        case meshes
+        case nodes
+        case samplers
+        case _scene = "scene"
+        case scenes
+        case skins
+        case textures
+        case extensions
+        case extras
+    }
 }
---
+++ Sources/VRM/Sampler.swift
@@ -0,0 +1,59 @@
+//
+//  Sampler.swift
+//  VRMKit
+//
+//  Created by Tatsuya Tanaka on 20180909.
+//  Copyright © 2018年 tattn. All rights reserved.
+//
+
+import Foundation
+
+// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#sampler
+
+extension GLTF {
+    public struct Sampler: Codable {
+        public let magFilter: MagFilter?
+        public let minFilter: MinFilter?
+        let _wrapS: Wrap?
+        public var wrapS: Wrap {
+            return self._wrapS ?? .REPEAT
+        }
+        let _wrapT: Wrap?
+        public var wrapT: Wrap {
+            return self._wrapT ?? .REPEAT
+        }
+        public let name: String?
+        public let extensions: Extensions?
+        public let extras: Extensions?
+
+        private enum CodingKeys: String, CodingKey {
+            case magFilter
+            case minFilter
+            case _wrapS = "wrapS"
+            case _wrapT = "wrapT"
+            case name
+            case extensions
+            case extras
+        }
+
+        public enum MagFilter: Int, Codable {
+            case NEAREST = 9728
+            case LINEAR = 9729
+        }
+
+        public enum MinFilter: Int, Codable {
+            case NEAREST = 9728
+            case LINEAR = 9729
+            case NEAREST_MIPMAP_NEAREST = 9984
+            case LINEAR_MIPMAP_NEAREST = 9985
+            case NEAREST_MIPMAP_LINEAR = 9986
+            case LINEAR_MIPMAP_LINEAR = 9987
+        }
+
+        public enum Wrap: Int, Codable {
+            case CLAMP_TO_EDGE = 33071
+            case MIRRORED_REPEAT = 33648
+            case REPEAT = 10497
+        }
+    }
+}
