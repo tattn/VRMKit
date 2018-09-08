e4d442ef35b9b6b9a0a5c4d47e4e73443efffb56
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2018-09-08 22:20:53 +0900

    Add extensions

---
+++ Sources/VRM/Extensions.swift
@@ -0,0 +1,94 @@
+//
+//  Extensions.swift
+//  VRMKit
+//
+//  Created by Tatsuya Tanaka on 20180908.
+//  Copyright © 2018年 tattn. All rights reserved.
+//
+
+import Foundation
+
+// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md#specifying-extensions
+
+public struct Extensions {
+    public let value: Any
+
+    public init(_ value: Any) {
+        self.value = value
+    }
+}
+
+extension Extensions: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if container.decodeNil() {
+            self.init(())
+        } else if let bool = try? container.decode(Bool.self) {
+            self.init(bool)
+        } else if let int = try? container.decode(Int.self) {
+            self.init(int)
+        } else if let uint = try? container.decode(UInt.self) {
+            self.init(uint)
+        } else if let double = try? container.decode(Double.self) {
+            self.init(double)
+        } else if let string = try? container.decode(String.self) {
+            self.init(string)
+        } else if let array = try? container.decode([Extensions].self) {
+            self.init(array.map { $0.value })
+        } else if let dictionary = try? container.decode([String: Extensions].self) {
+            self.init(dictionary.mapValues { $0.value })
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "failed to decode")
+        }
+    }
+}
+
+extension Extensions: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch value {
+        case is Void:
+            try container.encodeNil()
+        case let bool as Bool:
+            try container.encode(bool)
+        case let int as Int:
+            try container.encode(int)
+        case let int8 as Int8:
+            try container.encode(int8)
+        case let int16 as Int16:
+            try container.encode(int16)
+        case let int32 as Int32:
+            try container.encode(int32)
+        case let int64 as Int64:
+            try container.encode(int64)
+        case let uint as UInt:
+            try container.encode(uint)
+        case let uint8 as UInt8:
+            try container.encode(uint8)
+        case let uint16 as UInt16:
+            try container.encode(uint16)
+        case let uint32 as UInt32:
+            try container.encode(uint32)
+        case let uint64 as UInt64:
+            try container.encode(uint64)
+        case let float as Float:
+            try container.encode(float)
+        case let double as Double:
+            try container.encode(double)
+        case let string as String:
+            try container.encode(string)
+        case let date as Date:
+            try container.encode(date)
+        case let url as URL:
+            try container.encode(url)
+        case let array as [Any]:
+            try container.encode(array.map(Extensions.init))
+        case let dictionary as [String: Any]:
+            try container.encode(dictionary.mapValues(Extensions.init))
+        default:
+            throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: container.codingPath, debugDescription: "failed to encode"))
+        }
+    }
+}
---
+++ Tests/ExtensionsTests.swift
@@ -0,0 +1,47 @@
+//
+//  ExtensionsTests.swift
+//  VRMKitTests
+//
+//  Created by Tatsuya Tanaka on 20180908.
+//  Copyright © 2018年 tattn. All rights reserved.
+//
+
+import XCTest
+import VRMKit
+
+class ExtensionsTests: XCTestCase {
+
+    let jsonEncoder = JSONEncoder()
+    let jsonDecoder = JSONDecoder()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testInt() {
+        encodeAndDecode(["value": 1])
+    }
+
+    func testStringIntDictionary() {
+        encodeAndDecode(["value": ["key": 1]])
+    }
+
+    func testDoubleArray() {
+        encodeAndDecode([1.1, 2.2, 3.3])
+    }
+
+    func testNestedArray() {
+        encodeAndDecode([[[["one", "two", "three"]]]])
+    }
+
+    private func encodeAndDecode<T: Equatable>(_ expectedValue: T) {
+        let extensions = Extensions(expectedValue)
+        let data = try! jsonEncoder.encode(extensions)
+        let decodedValue = try! jsonDecoder.decode(Extensions.self, from: data)
+        XCTAssertEqual(decodedValue.value as! T, expectedValue)
+    }
+}
