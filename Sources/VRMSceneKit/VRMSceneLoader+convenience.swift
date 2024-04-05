168879ae4bde92650fde5ecc1b981bb1dd8f99a4
Yoshinori Takada <is0263rf@gmail.com>
2024-04-05 15:47:00 +0900

    Remove type annotations.

--- Sources/VRMSceneKit/VRMSceneLoader+convenience.swift
+++ Sources/VRMSceneKit/VRMSceneLoader+convenience.swift
@@ -11,17 +11,17 @@ import Foundation
 
 extension VRMSceneLoader {
     public convenience init(withURL url: URL, rootDirectory: URL? = nil) throws {
-        let vrm: VRM = try VRMLoader().load(withURL: url)
+        let vrm = try VRMLoader().load(withURL: url)
         self.init(vrm: vrm, rootDirectory: rootDirectory)
     }
 
     public convenience init(named: String, rootDirectory: URL? = nil) throws {
-        let vrm: VRM = try VRMLoader().load(named: named)
+        let vrm = try VRMLoader().load(named: named)
         self.init(vrm: vrm, rootDirectory: rootDirectory)
     }
 
     public convenience init(withData data: Data, rootDirectory: URL? = nil) throws {
-        let vrm: VRM = try VRMLoader().load(withData: data)
+        let vrm = try VRMLoader().load(withData: data)
         self.init(vrm: vrm, rootDirectory: rootDirectory)
     }
 }
