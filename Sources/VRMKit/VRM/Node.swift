f638a38edc71cb388461705ce44df8a21f5dd811
Yoshinori Takada <is0263rf@gmail.com>
2024-04-12 10:41:46 +0900

    Add parsing VRMC_node_constraint.

--- Sources/VRMKit/VRM/Node.swift
+++ Sources/VRMKit/VRM/Node.swift
@@ -34,7 +34,7 @@ extension GLTF {
         }
         public let weights: [Float]?
         public let name: String?
-        public let extensions: CodableAny?
+        public let extensions: NodeExtensions?
         public let extras: CodableAny?
 
         private enum CodingKeys: String, CodingKey {
@@ -50,6 +50,86 @@ extension GLTF {
             case name
             case extensions
             case extras
+        }
+
+        public struct NodeExtensions: Codable {
+            public let nodeConstraint: NodeConstraint?
+
+            private enum CodingKeys: String, CodingKey {
+                case nodeConstraint = "VRMC_node_constraint"
+            }
+
+            public struct NodeConstraint: Codable {
+                public let specVersion: String
+                public let constraint: Constraint
+
+                public struct Constraint: Codable {
+                    public let roll: RollConstraint?
+                    public let aim: AimConstraint?
+                    public let rotation: RotationConstraint?
+
+                    public struct RollConstraint: Codable {
+                        public let source: Int
+                        public let rollAxis: RollAxis
+                        public let weight: Double?
+
+                        public enum RollAxis: String, Codable {
+                            case X
+                            case Y
+                            case Z
+                        }
+
+                        public init(from decoder: Decoder) throws {
+                            let container = try decoder.container(keyedBy: CodingKeys.self)
+                            source = try container.decode(Int.self, forKey: .source)
+                            rollAxis = try container.decode(RollAxis.self, forKey: .rollAxis)
+                            weight = try? decodeDouble(key: .weight, container: container)
+                        }
+                    }
+
+                    public struct AimConstraint: Codable {
+                        public let source: Int
+                        public let aimAxis: AimAxis
+                        public let weight: Double?
+
+                        public enum AimAxis: String, Codable {
+                            case positiveX
+                            case negativeX
+                            case positiveY
+                            case negativeY
+                            case positiveZ
+                            case negativeZ
+
+                            private enum CodingKeys: String, CodingKey {
+                                case positiveX = "PositiveX"
+                                case negativeX = "NegativeX"
+                                case positiveY = "PositiveY"
+                                case negativeY = "NegativeY"
+                                case positiveZ = "PositiveZ"
+                                case negativeZ = "NegativeZ"
+                            }
+                        }
+
+                        public init(from decoder: Decoder) throws {
+                            let container = try decoder.container(keyedBy: CodingKeys.self)
+                            source = try container.decode(Int.self, forKey: .source)
+                            aimAxis = try container.decode(AimAxis.self, forKey: .aimAxis)
+                            weight = try? decodeDouble(key: .weight, container: container)
+                        }
+                    }
+
+                    public struct RotationConstraint: Codable {
+                        public let source: Int
+                        public let weight: Double?
+
+                        public init(from decoder: Decoder) throws {
+                            let container = try decoder.container(keyedBy: CodingKeys.self)
+                            source = try container.decode(Int.self, forKey: .source)
+                            weight = try? decodeDouble(key: .weight, container: container)
+                        }
+                    }
+                }
+            }
         }
     }
 }
